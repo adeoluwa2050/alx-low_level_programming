3c0b9c1fb7ac16ab7297c4ac9812a8889ceb3e52
adeolu ogunleye <ogunleyeea@gmail.com>
2022-09-09 23:11:13 -0700

    a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
-* main-printing
+* main-Printing
 *
-* Description: using the main function
-*
-* Return: Always 0 (successful)
+* Return: Always 0 (Successful)
 */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual
+puzzle");
     return (0);
 }
