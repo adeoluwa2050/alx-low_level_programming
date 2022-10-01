67965129a9476af82f8c92f315c06658d76477b2
adeolu ogunleye <ogunleyeea@gmail.com>
2022-10-01 04:53:17 -0700

    a function that fills memory with a constant byte

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,8 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdio.h>
+#include <stddef.h>
+
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -10,7 +11,7 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
-int _putchar(char c);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-#endi
+
+#endif
